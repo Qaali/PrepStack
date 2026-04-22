4f2cfa5dcc7a0bb58deb1fe403d57579b623b92c
Qaali <qali@ualberta.ca>
2026-04-21 21:15:19 -0600

    Added POST and DELETE methods to QuestionsController

--- PrepStack/Controllers/QuestionsController.cs
+++ PrepStack/Controllers/QuestionsController.cs
@@ -24,5 +24,40 @@ namespace PrepStack.Controllers
         {
             return _context.Questions.ToList();
         }
+
+        // This is a "POST" request - it receives data to save it
+        [HttpPost]
+        public ActionResult<Question> CreateQuestion(Question newQuestion)
+        {
+            // 1. Tell the Librarian to add the new question to the pile
+            _context.Questions.Add(newQuestion);
+
+            // 2. Tell the Librarian to actually go to the warehouse and save it
+            _context.SaveChanges();
+
+            // 3. Return the new question so we can see it was saved
+            return Ok(newQuestion);
+        }
+
+        [HttpDelete("{id}")] // This tells the API to expect an ID in the URL, like api/questions/1
+        public ActionResult DeleteQuestion(int id)
+        {
+            // 1. Tell the Librarian to find the specific book by its ID
+            var question = _context.Questions.Find(id);
+
+            // 2. If the book doesn't exist, tell the customer "Not Found"
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            // 3. Put it in the "Trash" pile
+            _context.Questions.Remove(question);
+
+            // 4. Empty the trash (Save changes to SQL)
+            _context.SaveChanges();
+         
+            return Ok("Question deleted successfully.");
+        }
     }
 }
